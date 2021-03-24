a13048b5ea4028f39d33b5003e2844571b3d95bd
perhapsmaybe <68890108+perhapsmaybe@users.noreply.github.com>
2021-03-23 17:52:55 -0700

    Update BladeQuest.lua

--- BladeQuest.lua
+++ BladeQuest.lua
@@ -1,285 +1,102 @@
-repeat 
-    wait()
-until game:IsLoaded()
+function GameLoaded() repeat wait() until game:IsLoaded() end
+function CheckGame()if game.PlaceId==6494523288 then return true,false end;if game.gameId==2429242760 then return false,true else return false,false end end
+function ConvertFunctions()local a={["writefile"]="None",["readfile"]="None",["isfile"]="None"}if is_synapse_function then a["writefile"]=writefile;a["readfile"]=readfile;a["isfile"]=isfile;return a elseif pebc_execute then a["writefile"]=writefile;a["readfile"]=readfile;a["isfile"]=function(b)local c,d=pcall(function()a["readfile"](b)end)if c==false or d~=nil and string.find(d,"file does not exist")or d~=nil and string.find(d,"attempt to index nil with 'readfile'")then return false elseif c==true and d==nil then return true end end;return a elseif issentinelclosure then a["writefile"]=writefile;a["readfile"]=readfile;a["isfile"]=function(b)local c,d=pcall(function()a["readfile"](b)end)if c==false or d~=nil and string.find(d,"file does not exist")or d~=nil and string.find(d,"attempt to index nil with 'readfile'")then return false elseif c==true and d==nil then return true end end;return a else return false end end 
 
-if game.PlaceId == 6494529140 or game.PlaceId == 6494523288 or game.gameId == 2429242760 then 
-    if isfolder and isfile and writefile and readfile and makefolder then
-        if isfolder("Blade Quest") then
-            if isfile("Blade Quest/Settings.txt") then
-                local Settings = game:GetService("HttpService"):JSONDecode(readfile("Blade Quest/Settings.txt"))
-                
-                shared["CreateDungeon"] = Settings["CreateDungeon"]
-                shared["DungeonFriends"] = Settings["DungeonFriends"]
-                shared["DungeonHardcore"] = Settings["DungeonHardcore"]
-                shared["DungeonMap"] = Settings["DungeonMap"]
-                shared["DungeonDifficulty"] = Settings["DungeonDifficulty"]
-                shared["InstaKill"] = Settings["InstaKill"]
-                shared["InstaKillTP"] = Settings["InstaKillTP"]
-                shared["AutoEquipBest"] = Settings["AutoEquipBest"]
-            else
-                writefile("Blade Quest/Settings.txt", game:GetService("HttpService"):JSONEncode(
-                    {
-                        ["CreateDungeon"] = false,
-                        ["DungeonFriends"] = false,
-                        ["DungeonHardcore"] = false,
-                        ["DungeonMap"] = "Forest",
-                        ["DungeonDifficulty"] = "Easy",
-                        ["InstaKill"] = false,
-                        ["InstaKillTP"] = false,
-                        ["AutoEquipBest"] = false
-                    }))
-            end 
-        else
-            makefolder("Blade Quest")
-            
-            writefile("Blade Quest/Settings.txt", game:GetService("HttpService"):JSONEncode(
-                    {
-                        ["CreateDungeon"] = false,
-                        ["DungeonFriends"] = false,
-                        ["DungeonHardcore"] = false,
-                        ["DungeonMap"] = "Forest",
-                        ["DungeonDifficulty"] = "Easy",
-                        ["InstaKill"] = false,
-                        ["InstaKillTP"] = false,
-                        ["AutoEquipBest"] = false
-                    }))
-        end     
-    else
-        game.Players.LocalPlayer:Kick("Not supported exploit, please use Synapse X.")
-    end 
-    
-    function UpdateSettings()
-        writefile("Blade Quest/Settings.txt", game:GetService("HttpService"):JSONEncode({
-            ["CreateDungeon"] = shared["CreateDungeon"],
-            ["DungeonFriends"] = shared["DungeonFriends"],
-            ["DungeonHardcore"] = shared["DungeonHardcore"],
-            ["DungeonMap"] = shared["DungeonMap"],
-            ["DungeonDifficulty"] = shared["DungeonDifficulty"],
-            ["InstaKill"] = shared["InstaKill"],
-            ["InstaKillTP"] = shared["InstaKillTP"],
-            ["AutoEquipBest"] = shared["AutoEquipBest"]
-        }))
-    end 
+local Workspace = game:GetService("Workspace")
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ConvertedFunctions = ConvertFunctions()
 
-    local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
-    local UI = Material.Load({Title="Blade Quest", Style=_G.UIStyle or 3, SizeX=400, SizeY=300, Theme="Light"})
-    
-    local Player = UI.New({
-        Title = "Player"
-    })
-    
-    local Dungeon = UI.New({
-        Title = "Dungeon"
-    })
-    
-    repeat wait() until game:IsLoaded()
-    
-    Dungeon.Toggle({
-        Text = "Create Dungeon",
-        Callback = function(Value)
-            shared.CreateDungeon = Value
-            UpdateSettings()
-            
-            spawn(
-                function()
-                    repeat
-                        wait(5)
-                        if shared.CreateDungeon and game.PlaceId == 6494523288 then
-                            local Map = shared.DungeonMap or "Forest"
-                            local Difficulty = shared.DungeonDifficulty or "Easy"
-                            local Friends = shared.DungeonFriends or true 
-                            local Hardcore = shared.DungeonHardcore or false
-                                
-                            print(Map)
-                            print(Difficulty)
-                            print(Friends)
-                            print(Hardcore)
-                            game:GetService("ReplicatedStorage").RF:InvokeServer("Start")
-                            game:GetService("ReplicatedStorage").RF:InvokeServer("Create", Map, Difficulty, Friends, Hardcore)
-                            game:GetService("ReplicatedStorage").RF:InvokeServer("Start")
-                        end 
-                    until shared.CreateDungeon == false 
-                end 
-            )
-        end,
-        Enabled = shared["CreateDungeon"] or false
-    })
-    
-    Dungeon.Toggle({
-        Text = "Friends Only",
-        Callback = function(Value)
-            shared.DungeonFriends = Value
-            UpdateSettings()
-        end,
-        Enabled = shared["DungeonFriends"] or false
-    })
-    
-    Dungeon.Toggle({
-        Text = "Hardcore",
-        Callback = function(Value)
-            shared.DungeonHardcore = Value
-            UpdateSettings()
-        end,
-        Enabled = shared["DungeonHardcore"] or false
-    })
-    
-    function GetMaps() 
-        local ReturnTable = {} 
-        if game.Players.LocalPlayer.PlayerGui:FindFirstChild("UI") and game.Players.LocalPlayer.PlayerGui.UI:FindFirstChild("Play") and game.Players.LocalPlayer.PlayerGui.UI.Play:FindFirstChild("Create") and game.Players.LocalPlayer.PlayerGui.UI.Play:FindFirstChild("Create") and game.Players.LocalPlayer.PlayerGui.UI.Play.Create:FindFirstChild("Maps") then 
-            for i, v in pairs(game.Players.LocalPlayer.PlayerGui.UI.Play.Create.Maps:GetChildren()) do 
-                local PlayerLevel = game.Players.LocalPlayer.leaderstats.Level.Value
-                if v.Name ~= "Soon" and v.Name ~= "UIGridLayout"  then
-                    local MapLevel = string.split(v.Lvl.Text, "Level ")[2]
-                    MapLevel = MapLevel:gsub("+", "")
-                    if v:FindFirstChild("Lvl") and tonumber(MapLevel) <= PlayerLevel then 
-                        table.insert(ReturnTable, v.Name) 
-                    end 
-                end 
-            end 
-        end 
-        return ReturnTable 
-    end 
-    
-    function GetDifficulties() 
-        local Difficulties = {["Easy"] = 1, ["Medium"] = 4, ["Hard"] = 7, ["Expert"] = 10}
+shared.BladeQuest = {
+    ["Player"] = {
+        ["AutoAttack"] = false,
+    },
+    ["Dungeon"] = {
+        ["CreateDungeon"] = true,
+        ["Map"] = "Forest",
+        ["Difficulty"] = "Easy",
+        ["Hardcore"] = false,
+        ["FriendsOnly"] = true
+    },
+    ["Pathfinding"] = {
+        ["Pathfinding"] = true,
+        ["ShowPath"] = false,
+        ["AroundObstacles"] = false
+    },
+    ["AutoBuy"] = {
+        ["AutoBuy"] = true,
+        ["MaxBuyCost"] = 5000,
+        ["BuyBestSword"] = true,
+        ["BuyTime"] = 1
+    },
+    ["AutoSell"] = {
+        ["AutoSell"] = false,
+        ["SellTime"] = 1,
+        ["MinSellValue"] = 10,
+        ["MaxSellValue"] = 10,
+        ["MinLevel"] = 1,
+        ["MaxLevel"] = 1
+    }
+}
+
+local Lobby, Dungeon = CheckGame()
+
+if not Lobby and not Dungeon then return end 
+if not ConvertedFunctions then Players.LocalPlayer:Kick("Incompatible exploit, as of now Synapse X, Sentinel, and Protosmasher are supported!") end 
+
+CheckGame()
+
+-- // Settings
+
+function LoadSettings()local a=ConvertedFunctions;if a.readfile and a.isfile and a.writefile then local b=a.readfile;local c=a.writefile;local d=a.isfile;if d("BladeQuest.txt")==false then c("BladeQuest.txt",game:GetService("HttpService"):JSONEncode(shared.BladeQuest))else local e=game:GetService("HttpService"):JSONDecode(b("BladeQuest.txt"))for f,g in pairs(e)do for h,i in pairs(g)do shared.BladeQuest[f][h]=i end end end else warn("Failed to load settings!")return false end end 
+function SaveSettings()local a=ConvertedFunctions;if a.readfile and a.isfile and a.writefile then local b=a.readfile;local c=a.writefile;local d=a.isfile;if d("BladeQuest.txt")==false then LoadSettings()else local e=game:GetService("HttpService"):JSONDecode(b("BladeQuest.txt"))local f={}for g,h in pairs(shared.BladeQuest)do f[g]={}for i,j in pairs(h)do f[g][i]=j end end;c("BladeQuest.txt",game:GetService("HttpService"):JSONEncode(f))end else warn("Failed to save settings!")return false end end  
+
+-- // UI 
+
+local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
+local UI = Material.Load({Title="Blade Quest", Style=_G.UIStyle or 3, SizeX=400, SizeY=300, Theme="Light"})
+local Player = UI.New({Title="Player"})
+local Dungeon = UI.New({Title="Dungeon"})
+local Pathfinding = UI.New({Title = "Pathfinding"})
+local AutoBuy = UI.New({Title="Auto-Buy"})
+local AutoSell = UI.New({Title="Auto-Sell"})
+
+LoadSettings()
+SaveSettings()
+
+local AutoAttack = Player.Toggle({Text="Auto-Attack",
+    Callback = function(Value)
+        shared.BladeQuest["Player"]["AutoAttack"] = Value
         
-        local PlayerLevel = game.Players.LocalPlayer.leaderstats.Level.Value
-        local ReturnTable = {} 
-        
-        for i, v in pairs(Difficulties) do
-            if v <= PlayerLevel then
-                table.insert(ReturnTable, i)
-            end 
-        end 
-        
-        return ReturnTable 
-    end
-    
-    table.foreach(GetMaps(), warn)
-    local Dropdown1 = Dungeon.Dropdown({
-        Text = "Dungeon Map",
-        Callback = function(Value)
-            shared.DungeonMap = Value
-            UpdateSettings()
-        end,
-        Options = GetMaps()
-    })
-
-    Dropdown1:SetText(shared["DungeonMap"] or "Forest")
-    
-    local Dropdown2 = Dungeon.Dropdown({
-        Text = "Difficulty",
-        Callback = function(Value)
-            shared.DungeonDifficulty = Value
-            UpdateSettings()
-        end,
-        Options = GetDifficulties()
-    })
-
-    Dropdown2:SetText(shared["DungeonDifficulty"] or "Easy")
-    
-    function CheckObject(Object) if Object ~= nil then return true end return false end
-    
-    function Loaded() return game:IsLoaded() end
-    function LocalPlayer() return Loaded() and game:GetService("Players").LocalPlayer end 
-    function PlayerAlive() if not Loaded() or not LocalPlayer() then return false end if CheckObject(game:GetService("Players").LocalPlayer.Character) and game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid") and game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid").Health > 0 then return true end return false end
-    
-    shared.KillingEnemies = false
-    Player.Toggle({
-        Text = "Insta-Kill",
-        Callback = function(Value)
-            shared.InstaKill = Value
-            UpdateSettings()
-            spawn(
-                function()
-                    repeat
-                        wait(1)
-                        if shared.InstaKill and not shared.KillingEnemies then
-                            if game:GetService("Workspace"):FindFirstChild("Enemies") then
-                                local OrginalTable = game:GetService("Workspace"):FindFirstChild("Enemies"):GetChildren()
-                                for i1, v1 in pairs(game:GetService("Workspace"):FindFirstChild("Enemies"):GetChildren()) do
-                                    if i1 == #OrginalTable then
-                                        shared.KillingEnemies = false
-                                        
-                                        if PlayerAlive() then
-                                            for i, v in pairs(game:GetService("Players").LocalPlayer.Character:GetChildren()) do
-                                                if v.Name == "HumanoidRootPart" then
-                                                    v.Anchored = false 
-                                                end 
-                                            end          
-                                        end
-                                    else
-                                        shared.KillingEnemies = true    
-                                    end 
-                                    
-                                    if v1 and v1:FindFirstChild("HumanoidRootPart") then
-                                        if v1~= nil and PlayerAlive() and v1:FindFirstChild("Enemy") and v1:FindFirstChild("Enemy").Health > 0 and v1.Name ~= "Dummy" then
-                                            for i, v in pairs(game:GetService("Players").LocalPlayer.Character:GetChildren()) do
-                                                if v.Name == "HumanoidRootPart" then
-                                                    v.Anchored = false 
-                                                end 
-                                            end   
-                                            
-                                            if shared.InstaKillTP then
-                                                game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = v1.HumanoidRootPart.CFrame + Vector3.new(0, 5, 0) 
-                                            end 
-                                            
-                                            game:GetService("ReplicatedStorage").RE:FireServer("Hit", v1, v1.HumanoidRootPart.Position)
-                                            v1:FindFirstChild("Enemy").Health = 0
-                                            
-                                            wait(0.75)
-                                        elseif v1:FindFirstChild("Enemy") and v1:FindFirstChild("Enemy").Health == 0 and v1.Name ~= "Dummy" then
-                                            wait(5)
-                                            if v1 ~= nil and v1:FindFirstChild("Enemy") and v1:FindFirstChild("Enemy").Health == 0 then
-                                                game:GetService("TeleportService"):Teleport(6494523288, game.Players.LocalPlayer)
-                                            end 
-                                        end 
-                                    end 
-                                end 
-                            end 
-                        end 
-                    until shared.InstaKill == false
-                end 
-            )
-        end, 
-        Enabled = shared["InstaKill"] or false
-    })
-    
-    Player.Toggle({
-        Text="Insta-Kill TP",
-        Callback = function(Value)
-            shared.InstaKillTP = Value
-            UpdateSettings()
-        end,
-        Enabled = shared["InstaKillTP"] or false
-    })
-    
-    Player.Toggle({
-        Text="Auto-Equip Best",
-        Callback = function(Value)
-            shared.AutoEquipBest = Value 
-            UpdateSettings()
-            
-            spawn(
-                function()
-                   repeat 
-                        wait(5)
-                        
-                        if PlayerAlive() and game.Players.LocalPlayer:FindFirstChild("Data") and game.Players.LocalPlayer.Data:FindFirstChild("Swords") then
-                            local HighSword = {0, "None"}
-                            for i, v in pairs(game.Players.LocalPlayer.Data.Swords:GetChildren()) do
-                                if v.Amnt.Value > HighSword[1] then
-                                    HighSword[2] = v 
-                                    HighSword[1] = v.Amnt.Value
-                                end 
-                            end 
-                            
-                            game:GetService("ReplicatedStorage").RE:FireServer("Equip", HighSword[2])
+        local a = getsenv(game.Players.LocalPlayer.Character.Sword.Sword)
+        setsimulationradius(1e3, 1e3)
+        spawn(
+            function()
+                repeat
+                    wait()
+                    if shared.BladeQuest["Player"]["AutoAttack"] then
+                        for i, v in pairs(workspace.Enemies:GetChildren()) do
+                            if v:FindFirstChildWhichIsA("BasePart") and (v:FindFirstChildWhichIsA("BasePart").Position - game.Players.LocalPlayer.Character.Head.Position).magnitude < 15 and v:FindFirstChildOfClass("Humanoid").Health ~= 0 then
+                                --game:GetService("ReplicatedStorage").RE:FireServer("Hit", v, v:FindFirstChildWhichIsA("BasePart").Position)
+                                a.Shoot()
+                                a.ResetSpeed()
+                            end
                         end
-                    until shared.AutoEquipBest == false
-                end 
-            )
-        end,
-        Enabled = shared["AutoEquipBest"] or false
-    })
-end 
+                    end 
+                until shared.BladeQuest["Player"]["AutoAttack"] == false
+            end 
+        )
+    end, 
+    Enabled = shared.BladeQuest["Player"]["AutoAttack"]
+})
+
+-- // Dungeon 
+
+local CreateDungeon = Dungeon.Toggle({Text="Create Dungeon", 
+    Callback = function(Value)
+        shared.BladeQuest["Dungeon"]["CreateDungeon"] = Value 
+        SaveSettings()
+    end,
+    Enabled = shared.BladeQuest["Dungeon"]["CreateDungeon"]
+})
+
